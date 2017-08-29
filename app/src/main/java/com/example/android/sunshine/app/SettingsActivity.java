f2ca9fc20aca5ed2e6437c980411eb20392a2720
Peter Wise <peter@bloodaxe.com>
2017-08-29 18:43:10 +0100

    copyback some of their code

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -88,7 +88,4 @@ public class SettingsActivity extends PreferenceActivity
     public Intent getParentActivityIntent() {
         return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
-
-
-
 }
